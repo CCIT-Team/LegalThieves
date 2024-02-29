14cbfa2454568d2c5cfc425f089f0a483186eecd
LAPTOP-JSNC7LV6\jimin <6inggni9@naver.com>
2024-02-29 14:40:22 +0900

    플레이어 이동 및 1인칭 시점

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 inputVec;
+    public float speed = 5f;
+
+    Rigidbody rigid;
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = inputVec;
+        transform.position += Camera.main.transform.TransformDirection(inputVec) * speed * Time.deltaTime;
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector3>();
+    }
+}
