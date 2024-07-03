f916881b242bf027639a56bb14ccc2720c841cda
LAPTOP-7MAQMBV3\user <rhtjdwns00@naver.com>
2024-07-03 14:23:42 +0900

    유물 생성 로직 완료. 방 하나당 한번작동 시켜야 합니다. RoomValue확인후 값에 따라 확률표를 적용해서 생성될 유물 선택후 RoomType 값을 확인후 한번더 거릅니다. 스크립트3종류, Artifact.cs(Relics 생성),Room.cs(임시 방의 백터리스트,id,value,type 생성),item.cs(유물의 정보) 유물의 대용으로 item.cs를 가진 cube를 prefab에 추가했습니다. 확률표는 Artifact.cs의 인스펙터 창의 probability tables을 이용하여 추가 제거 가능합니다.

--- Assets/Scripts/item.cs
+++ Assets/Scripts/item.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.ShaderGraph.Internal;
 
-
-public class Item : MonoBehaviour
+[System.Serializable]
+public class Item : MonoBehaviour //유물이 가지는 값
 {
-    public enum Type { Artifact, ArtifactLocation };
+    public enum Type { Relics, goldRelics, renowonRelics }; //유물의 종류
     public Type type;
-    public float price;  // 유물의 가격
     public int year;   // 유물이 발견된 년도
-    public float weight;  // 유물의 무게
+    public int AllPoint; // 유물의 총 포인트
+    public int GlodPoint;  // 유물의 가격
+    public int RenownPoint;  // 유물의 명성
+    public int RoomID;    // 방의ID
 }
