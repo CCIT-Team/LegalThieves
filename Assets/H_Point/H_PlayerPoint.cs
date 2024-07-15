48294fe309f45dfc010935a92366a2529d95af87
허찬\hurch <hurchanqwer@naver.com>
2024-07-15 23:15:12 +0900

    Temp Player 로 변경

--- Assets/H_Point/H_PlayerPoint.cs
+++ Assets/H_Point/H_PlayerPoint.cs
@@ -1,61 +1,40 @@
 using Fusion;
+using LegalThieves;
 using System;
 
 using UnityEngine;
 
 public class H_PlayerPoint : NetworkBehaviour
 {
-    //GameLogic에 넣어야하는것
-
-    public H_PlayerPoint[] playerScript;
-    public void SetRank()
-    {
-        int[] RankArray = new int[4];
-        for (int i = 0; i < playerScript.Length;)
-        {
-            RankArray[i] = playerScript[i].winPoint;
-        }
-        Array.Sort(RankArray);// 인덱스 0부터 최고점수 즉, 1등
-        
-        foreach (var rank in RankArray)
-        {
-            Debug.Log(rank);
-            //순위 로직
-        }
-
-    }
-
     //player가 가지고 있어야할것
     public enum GoldOrRenown { Gold, Renown }
-    GoldOrRenown EPlayerWinPoint;
-    public int[] inventory;
+    public GoldOrRenown EPlayerWinPoint;
+
+    
 
     //임시로 유물 리스트 구현
-    private Relic[] relics;
 
-
-    private void Start()
+    public override void Spawned()
     {
-        inventory = new int[10];
+        //인벤토리 설정
+        //inventory = new int[10]; 테스트를 위해 주석처리
+      
         remainPoint = maxPoint;
     }
 
-    Relic relic;
+    [Networked] public int goldPoint { get; set; }
+    [Networked] public int renownPoint { get; set; }
+    [Networked] public int buyPoint { get; set; }
+    [Networked] public int remainPoint { get; set; }
+    [Networked] public int maxPoint { get; set; }
+    [Networked] public int winPoint { get; set; }
 
-
-    [Networked] private int goldPoint { get; set; }
-    [Networked] private int renownPoint { get; set; }
-    [Networked] private int buyPoint { get; set; }
-    [Networked] private int remainPoint { get; set; }
-    [Networked] private int maxPoint { get; set; }
-    [Networked] private int winPoint { get; set; }
-
-    public int GoldPoint { get { return goldPoint; } }
-    public int RenownPoint { get { return renownPoint; } }
-    public int BuyPoint { get { return buyPoint; } }
-    public int RemainPoint { get { return remainPoint; } }
-    public int MaxPoint { get { return maxPoint; } }
-    public int WinPoint { get { return winPoint; } }
+    //public int GoldPoint { get { return goldPoint; } }
+    //public int RenownPoint { get { return renownPoint; } }
+    //public int BuyPoint { get { return buyPoint; } }
+    //public int RemainPoint { get { return remainPoint; } }
+    //public int MaxPoint { get { return maxPoint; } }
+    //public int WinPoint { get { return winPoint; } }
     void SetRemain()// 주울수 있는 양을 설정
     {
         remainPoint = remainPoint - (goldPoint + renownPoint);
@@ -86,31 +65,7 @@ public class H_PlayerPoint : NetworkBehaviour
         Debug.Log("더이상 주울 수 없습니다");
     }
 
-    void EndRound(GoldOrRenown playerWinPoint)
-    {
-        //Relic temp;
-        //foreach (int i in inventory)
-        //{
-        //    // temp = Relic[i].goldPoint     인벤토리의 인덱스에 따라 렐릭의 데이터를 가져옴
-        //}
-
-        switch (playerWinPoint)
-        {
-            case GoldOrRenown.Gold:
-
-                winPoint = goldPoint; // 기존 
-                buyPoint = renownPoint;
-                break;
-
-            case GoldOrRenown.Renown:
-                winPoint = renownPoint;
-                buyPoint = goldPoint;
-                break;
-        }
-
-
-        SetRemain();   //남은 스코어 
-    }
+   
 
 
 }
