edca02306772f6e0d90ea48b298dea0060a6ee32
허찬\hurch <hurchanqwer@naver.com>
2024-07-17 03:07:20 +0900

    gamelogic 에 변수가 있다는 전제로 수정

--- Assets/H_Point/H_PlayerPoint.cs
+++ Assets/H_Point/H_PlayerPoint.cs
@@ -10,17 +10,6 @@ public class H_PlayerPoint : NetworkBehaviour
     public enum GoldOrRenown { Gold, Renown }
     public GoldOrRenown EPlayerWinPoint;
 
-    
-
-    //임시로 유물 리스트 구현
-
-    public override void Spawned()
-    {
-        //인벤토리 설정
-        //inventory = new int[10]; 테스트를 위해 주석처리
-      
-        remainPoint = maxPoint;
-    }
 
     [Networked] public int goldPoint { get; set; }
     [Networked] public int renownPoint { get; set; }
@@ -29,18 +18,14 @@ public class H_PlayerPoint : NetworkBehaviour
     [Networked] public int maxPoint { get; set; }
     [Networked] public int winPoint { get; set; }
 
+
     //public int GoldPoint { get { return goldPoint; } }
     //public int RenownPoint { get { return renownPoint; } }
     //public int BuyPoint { get { return buyPoint; } }
     //public int RemainPoint { get { return remainPoint; } }
     //public int MaxPoint { get { return maxPoint; } }
     //public int WinPoint { get { return winPoint; } }
-    void SetRemain()// 주울수 있는 양을 설정
-    {
-        remainPoint = remainPoint - (goldPoint + renownPoint);
-        //UI에 바로 표현
-    }
-
+   
     void GetRelic(Relic relic) // 렐릭을 주웠을 때
     {
         if (remainPoint - relic.goldPoint - relic.renownPoint < 0) // 못주울 때
@@ -48,8 +33,6 @@ public class H_PlayerPoint : NetworkBehaviour
 
         goldPoint += relic.goldPoint;
         renownPoint += relic.renownPoint;
-
-        SetRemain();
     }
 
     void ThrowRelic(Relic relic)
@@ -57,7 +40,6 @@ public class H_PlayerPoint : NetworkBehaviour
         goldPoint -= relic.goldPoint;
         renownPoint -= relic.renownPoint;
 
-        SetRemain();
     }
 
     void PointOver() // 더 못주울 때
