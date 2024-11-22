74e5f8a3f10252659f96b8b35b3a90af339d4b12
HEOCHANNEO\hurch <hurchanqwer@gmail.com>
2024-11-22 19:08:38 +0900

    temp merge

--- Assets/New Neo LT/Scripts/UI/InventorySlotController.cs
+++ Assets/New Neo LT/Scripts/UI/InventorySlotController.cs
@@ -2,7 +2,7 @@
 using LegalThieves;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections;
 namespace New_Neo_LT.Scripts.UI
 {
     public class InventorySlotController : MonoBehaviour
@@ -10,8 +10,18 @@ namespace New_Neo_LT.Scripts.UI
         [SerializeField] private GameObject[] slots;
         private int _prevIndex;
 
+        [Header("Inventory UI")]
+        [SerializeField] private RectTransform inventoryUI; // 인벤토리 UI RectTransform
+        [SerializeField] private Vector3 hiddenPosition;// 화면 밖 위치
+        [SerializeField] private Vector3 visiblePosition;
+        private bool isInventoryOpen = true; // 인벤토리 열림 상태
+        private bool isCooldownActive = false; // 쿨다운 상태
+        WaitForSeconds  wait_0_3=  new WaitForSeconds(0.1f);
+
         private void Start()
         {
+            visiblePosition = inventoryUI.GetComponent<RectTransform>().anchoredPosition ;
+            hiddenPosition = visiblePosition + Vector3.left * 500;
             SelectToggle(0);
         }
 
@@ -33,6 +43,24 @@ namespace New_Neo_LT.Scripts.UI
             var slotImage = slots[index].transform.GetChild(2).GetComponent<Image>();
             slotImage.sprite = RelicManager.Instance.GetRelicSprite(RelicManager.Instance.GetRelicData(relicIndex).GetTypeIndex());
             slotImage.enabled = true;
+        }
+        public void OnToggleInventory()
+        {
+            if (!isCooldownActive) // 쿨다운 중이 아니면 동작
+            {
+                isCooldownActive = true; // 쿨다운 시작
+                StartCoroutine(ToggleInventoryCoroutine());
+            }
+        }
+
+        private IEnumerator ToggleInventoryCoroutine()
+        {
+            isInventoryOpen = !isInventoryOpen; // 상태 반전
+            inventoryUI.anchoredPosition = isInventoryOpen ? visiblePosition : hiddenPosition; // UI 위치 변경
+            Debug.Log($"Inventory is now {(isInventoryOpen ? "open" : "closed")}.");
+
+            yield return wait_0_3;
+            isCooldownActive = false; // 쿨다운 해제   
         }
     }
 }
