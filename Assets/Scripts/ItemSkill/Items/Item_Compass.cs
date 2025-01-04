ef2912cbfdee70ca4e931a372a3802ec6e773a6f
HURCHANQWER\hurch <hurchanqwer@naver.com>
2025-01-05 07:44:28 +0900

    홀로그램 셰이더로 변경 및 나침반 수정

--- Assets/Scripts/ItemSkill/Items/Item_Compass.cs
+++ Assets/Scripts/ItemSkill/Items/Item_Compass.cs
@@ -1,11 +1,20 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
+using System;
+using System.Net;
+using System.IO;
 public class Item_Compass : ItemBase
 {
 //todo 네비메쉬 이용해서 길찾기 보여주는거 구현, 아이템 목록에 추가, 애니메이션 추가
     [SerializeField] GameObject CompassObject;
-      public override void Init()
+    [SerializeField] MeshFilter meshFilter;
+    [SerializeField] Transform playerPos;
+    [SerializeField] Transform Entrans;
+    [SerializeField] Material pathMaterial;
+    Mesh pathMesh;
+    public override void Init()
     {
         ID = (int)EItemType.Compass;
     }
@@ -29,12 +38,64 @@ public class Item_Compass : ItemBase
 
     public void UsePathFinding(Animator animator)
     {
-  
             animator.SetTrigger("UseItem");
             canUse = false;
+            PathFinding();
+       
             StartCoroutine(CoolDownDelay());
        
-    }   
+    }
 
+    private void PathFinding()
+    {
+        NavMeshPath path = new NavMeshPath();
+        if (NavMesh.CalculatePath(playerPos.position, Entrans.position, NavMesh.AllAreas, path))
+        {
+            // 경로가 유효하면 Mesh 생성
+            UpdatePathMesh(path);
+        }
    
+    }
+    void UpdatePathMesh(NavMeshPath path)
+    {
+        // 경로 코너 가져오기
+        Vector3[] corners = path.corners;
+
+        // Vertex 및 Triangle 배열 생성
+        Vector3[] vertices = new Vector3[corners.Length * 2];
+        int[] triangles = new int[(corners.Length - 1) * 6];
+        float lineWidth = 0.2f; // 경로의 두께
+        Vector3 offset = Vector3.up * 0.1f; // 바닥에서 살짝 띄우기
+
+        for (int i = 0; i < corners.Length; i++)
+        {
+            Vector3 left = corners[i] - Vector3.right * lineWidth / 2;
+            Vector3 right = corners[i] + Vector3.right * lineWidth / 2;
+
+            vertices[i * 2] = left + offset;
+            vertices[i * 2 + 1] = right + offset;
+
+            if (i < corners.Length - 1)
+            {
+                int baseIndex = i * 6;
+                triangles[baseIndex] = i * 2;
+                triangles[baseIndex + 1] = i * 2 + 1;
+                triangles[baseIndex + 2] = i * 2 + 2;
+
+                triangles[baseIndex + 3] = i * 2 + 1;
+                triangles[baseIndex + 4] = i * 2 + 3;
+                triangles[baseIndex + 5] = i * 2 + 2;
+            }
+        }
+
+        // 미리 생성된 Mesh 업데이트
+        pathMesh = meshFilter.mesh;
+        pathMesh.Clear();
+        pathMesh.vertices = vertices;
+        pathMesh.triangles = triangles;
+        pathMesh.RecalculateNormals();
+
+        // Material 설정
+        meshFilter.GetComponent<MeshRenderer>().material = pathMaterial;
+    }
 }
