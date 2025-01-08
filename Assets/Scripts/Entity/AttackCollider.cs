879bd6d4c3e5a549bdc471f8963e6eb338b6289d
MoccaHazel <wltn1937@gmail.com>
2025-01-08 17:41:55 +0900

    박쥐 몬스터 추가

--- Assets/Scripts/Entity/AttackCollider.cs
+++ Assets/Scripts/Entity/AttackCollider.cs
@@ -1,17 +1,39 @@
+using Fusion;
 using New_Neo_LT.Scripts.PlayerComponent;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AttackColider : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PlayerCharacter>(out PlayerCharacter player))
         {
-            Debug.Log(other.gameObject.name + "'s points Down");
-            player.AddGoldPoint(-300);
-            player.AddRenownPoint(-300);
+            if(transform.root.TryGetComponent(out MonsterBatStateMachine bat))
+            {
+                Debug.Log(other.gameObject.name + "is stole relic");
+                List<int> reliclist = new List<int>();
+                for (int i = 0; i < 10; i++)
+                {
+                    int relicid = player.RelicInventory[i];
+                    if (relicid != -1 )
+                        reliclist.Add(relicid);
+                }
+                if (reliclist.Count > 0)
+                {
+                    bat.currentRelic = player.RelicInventory.IndexOf(reliclist[UnityEngine.Random.Range(0, reliclist.Count)]);
+                    player.RemoveRelicFromInventory(bat.currentRelic);
+                }
+            }
+            else
+            {
+                Debug.Log(other.gameObject.name + "'s points Down");
+                player.AddGoldPoint(-300);
+                player.AddRenownPoint(-300);
+            }
             gameObject.SetActive(false);
         }
     }
--- Assets/Scripts/Entity/States/PatrolState.cs
+++ Assets/Scripts/Entity/States/PatrolState.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion.Addons.FSM;
+using LegalThieves;
+using New_Neo_LT.Scripts.Game_Play;
 
 public class PatrolState : MonsterStateBase
 {
+    float patrolTime = 0;
     protected override void OnInitialize()
 	{
         
     }
     protected override void OnDeinitialize(bool hasState) { }
 
-    protected override void OnEnterState() { }
-    protected override void OnFixedUpdate() { }
+    protected override void OnEnterState()
+    {
+        monster.agent.isStopped = false;
+        monster.target = PlayerRegistry.GetRandom().transform;
+        patrolTime = Random.Range(0, 10);
+    }
+    protected override void OnFixedUpdate()
+    {
+
+    }
     protected override void OnExitState() { }
 
     protected override void OnEnterStateRender() { }
 
     protected override void OnRender() { }
     protected override void OnExitStateRender() { }
+
+    protected override bool CanExitState(StateBehaviour nextState)
+    {
+        return Machine.StateTime >= patrolTime;
+    }
 }
